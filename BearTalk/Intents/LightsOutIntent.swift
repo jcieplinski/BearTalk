a6aab0d3028e81b7c68820c749eb87c0811eb62b
Joe Cieplinski <joec@mac.com>
2023-12-25 11:25:42 -0700

    Updated icons and added lights out shortcut

---
+++ BearTalk/Intents/LightsOutIntent.swift
@@ -0,0 +1,18 @@
+//
+//  LightsOutIntent.swift
+//  BearTalk
+//
+//  Created by Joe Cieplinski on 12/25/23.
+//
+
+import AppIntents
+
+struct LightsOutIntent: AppIntent {
+    static var title: LocalizedStringResource = "Car Lights Out"
+
+    @MainActor func perform() async throws -> some ProvidesDialog {
+        let _ = try await BearAPI.refreshToken()
+        let _ = try await BearAPI.lightsControl(action: .off)
+        return .result(dialog: "Shutting down all lights…")
+    }
+}
